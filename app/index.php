3914846c568ff09eace91df68ada2fca3e12bc6a
harry siracuse <siracuse.harichandra@gmail.com>
2019-01-09 16:40:16 +0100

    remake

---
+++ app/index.php
@@ -0,0 +1,228 @@
+﻿<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Énergie collectives</title>
+    <link rel="stylesheet" href="css/style.css">
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+    <div style="width: 0; height: 0;">
+        <?php require 'svg/sprite_symbol_pictos.svg'; ?>
+    </div>
+</head>
+<body>
+    <header>
+
+        <div class="logo-header">
+            <img src="images/EnergiesCollectives_rectangle_Logo_ajuste.svg">
+
+        </div>
+
+<!--    MENU BURGER      -->
+        <div class="mobile-container">
+            <div class="topnav">
+                <div id="myLinks">
+                    <a href="#actualite">Actualités</a>
+                    <a href="#nousconnaitre">Nous connaître</a>
+                    <a href="#nosinstallation">Nos installations</a>
+                    <a href="#economisons">Economisons</a>
+                    <a href="#nosinstallation">Nos installations</a>
+                    <a href="#ensavoirplus">En savoir plus</a>
+                    <a href="#souscrire">Souscrire</a>
+                </div>
+                <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                    <img src="images/menu.png" style="width:20px">
+                </a>
+            </div>
+        </div>
+
+        <img class="bg-header" src="images/20161202_101925.jpg">
+    </header>
+
+    <main>
+        <section class="nousconnaitre">
+            <div class="logo-connaitre"><img src="images/LogoEnergiesCollectives_main.jpg"> </div>
+            <h1 class="titre">L'énergie est notre affaire <br>
+                <span>Investissons dans la transition énergétique</span>
+            </h1>
+            <div class="picto">
+                <div class="coop"><img src="images/picto_coop.svg"><h3>COOPÉRATEURS</h3> <h3 class="chiffre">190</h3> </div>
+                <div class="photo"><img src="images/picto_photovoltaique.svg" class="panneau"><h3>INSTALLATIONS</h3> <h3 class="chiffre">8</h3></div>
+                <div class="prod"><img src="images/picto_production_energie.svg"><h3>PRODUCTION <br>ANNUELLE</h3> <h3 class="chiffre">141 MWh</h3></div>
+                <div class="cap"><img src="images/picto_capital.svg"><h3>CAPITAL</h3> <h3 class="chiffre">112950 €</h3> </div>
+            </div>
+
+            <div class="content-connaitre">
+                <div class="text-connaitre">
+                    <h3>Produire et mieux consommer de l'électricité renouvelable et locale c'est possible !</h3>
+                    <div>
+                        <p>Habitants, entreprises et collectivités de Serre-Ponçon agissent ensemble,
+                        à travers la coopérative pour :</p>
+                        <ul>
+                            <li>Réduire les gaspillages énergétiques : consommer moins pour produire moins,</li>
+                            <li>Produire l'électricité localement : l'épargne citoyenne est investie dans des installations
+                                photovoltaïques, </li>
+                            <li> Agir ensemble pour l'intéret collectif : une personne = une voix.</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="bt-gris">
+                    <h2>Comprendre notre modèle de production</h2>
+                </div>
+
+                <div class="text-connaitre">
+                    <h3>Pourquoi devenir sociétaire d'Énergies Collectives ?</h3>
+                    <div>
+                    <p>
+                        Pourquoi passer à l’action et prendre une ou des part(s) sociale(s) de la coopérative ? Comment
+                        convaincre vos proches de nous rejoindre et d’investir dans la transition énergétique ? Quelques
+                        éléments de réflexion dans cet article, à faire circuler !
+                    </p>
+                    </div>
+                </div>
+
+                <div class="bt-orange">
+                    <h2>Souscrire ! <img src="images/schema_souscrire.svg"></h2>
+                </div>
+
+            </div>
+        </section>
+
+        <section class="actu">
+            <div class="actu-box">
+                <div class="head-actu">
+                    <h2>ACTUALITÉS</h2>
+                    <div>
+                        <img src="images/IMG_20170121_122143.jpg" class="actu-img">
+                    </div>
+                </div>
+                <div class="content">
+                    <div>
+                        <h2>Un 24 novembre de célébration</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="notredemarche">
+            <div class="demarche-box">
+                <div class="head-demarche">
+                    <h2>NOTRE DEMARCHE</h2>
+                    <div>
+                        <img src="images/IMG_20170121_122143.jpg" class="actu-img">
+                    </div>
+
+                </div>
+                <div class="content">
+                    <div>
+                        <h2>Un 24 novembre de célébration</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="nosinstallation">
+            <div class="installation-box">
+                <div class="head-installation">
+                    <h2>NOS INSTALLATIONS</h2>
+                    <div>
+                        <img src="images/IMG_20170121_122143.jpg" class="actu-img">
+                    </div>
+                </div>
+                <div class="content">
+                    <div>
+                        <h2>Un 24 novembre de célébration</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <div class="bt">
+            <a href="#">Voir toutes nos installations</a>
+        </div>
+
+        <section class="temoignage">
+            <div class="temoignage-box">
+                <div class="head-temoignage">
+                    <h2>TEMOIGNAGES</h2>
+                    <div>
+                        <img src="images/IMG_20170121_122143.jpg" class="actu-img">
+                    </div>
+                </div>
+                <div class="content">
+                    <div>
+                        <h2>Un 24 novembre de célébration</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
+    </main>
+
+    <footer>
+        <div class="grp-text">
+            <div class="text-footer">
+                <h2>Nous suivre</h2>
+            </div>
+            <div class="text-footer">
+                <h2>Nous contacter</h2>
+            </div>
+            <div class="text-footer">
+                <h2>Rechercher</h2>
+            </div>
+        </div>
+        <div class="text-footer">
+            <h2>Abonnez-vous à la Newsletter</h2>
+            <input type="text">
+        </div>
+        <div class="partenaire">
+            <span>Nos partenaires :</span>
+            <img src="images/partenaires/arton82.jpg">
+            <img src="images/partenaires/arton124.jpg">
+            <img src="images/partenaires/enercoop_logo.png">
+            <img src="images/partenaires/energie-partagee_logo.png">
+            <img src="images/partenaires/nef_logo.png">
+            <img src="images/partenaires/omnis_logo.png">
+            <img src="images/partenaires/paca_logo.png">
+            <img src="images/partenaires/parc-ecrins_logo.jpg">
+            <img src="images/partenaires/syme05_logo.png">
+            <img src="images/partenaires/udess-05_logo-seul.png">
+        </div>
+    </footer>
+    <script>
+        function myFunction() {
+            var x = document.getElementById("myLinks");
+            if (x.style.display === "flex") {
+                x.style.display = "none";
+            } else {
+                x.style.display = "flex";
+            }
+        }
+    </script>
+</body>
+</html>
--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,31 @@ gulp.task('sass', () => {
     }))
     .pipe(gulp.dest('app/css'))
 });
+var svgSprite    = require('gulp-svg-sprite');
+var plumber      = require('gulp-plumber');
+var baseDir      = 'app/images';   // <-- Set to your SVG base directory
+var svgGlob      = '**/*.svg';       // <-- Glob to match your SVG files
+var outDir       = 'app/svg';     // <-- Main output directory
+var config       = {
+    "shape": {
+        "spacing": {
+            "box": "icon"
+        }
+    },
+    "mode": {
+        "symbol": {
+            "dest": ".",
+            "sprite": "sprite_symbol_pictos.svg",
+        }
+    }
+};
 
+gulp.task('svgsprite', function() {
+    return gulp.src(svgGlob, {cwd: baseDir})
+        .pipe(plumber())
+        .pipe(svgSprite(config)).on('error', function(error){ console.log(error); })
+        .pipe(gulp.dest(outDir));
+});
 
 //Observateur
 gulp.task('watch', () => {
